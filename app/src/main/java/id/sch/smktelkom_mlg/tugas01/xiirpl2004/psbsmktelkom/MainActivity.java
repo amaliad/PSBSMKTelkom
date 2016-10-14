678aad9fb4c351b73191dfe78add5791c863ec6e
Amalia <amalia_damayanti_23rpl@student.smktelkom-mlg.sch.id>
2016-10-14 08:35:58 +0700

    Membuat Validasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2004/psbsmktelkom/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2004/psbsmktelkom/MainActivity.java
@@ -2,12 +2,44 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl2004.psbsmktelkom;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvHasil;
+    EditText etnama, ettmpLhr, etthnLhr, etasal;
+    CheckBox cbrgl, cbblg, cbaksl;
+    RadioGroup rgJK;
+    Spinner spagama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etnama = (EditText) findViewById(R.id.editTextNama);
+        ettmpLhr = (EditText) findViewById(R.id.editTexttmpLhr);
+        etthnLhr = (EditText) findViewById(R.id.editTextthnLhr);
+        etasal = (EditText) findViewById(R.id.editTextAsal);
+        cbrgl = (CheckBox) findViewById(R.id.checkBoxReguler);
+        cbblg = (CheckBox) findViewById(R.id.checkBoxBilingual);
+        cbaksl = (CheckBox) findViewById(R.id.checkBoxAksel);
+        rgJK = (RadioGroup) findViewById(R.id.RadioGroup);
+        spagama = (Spinner) findViewById(R.id.spinner);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonDaftar).setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        }));
+    }
+
+    private void doClick() {
+
     }
 }
