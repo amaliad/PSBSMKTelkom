b410944a6bb6d87e8f18c09403d0f47918b3df68
Amalia <amalia_damayanti_23rpl@student.smktelkom-mlg.sch.id>
2016-10-14 12:53:37 +0700

    Membuat validasi, jika data tidak diisi maka akan terjadi eror

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2004/psbsmktelkom/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2004/psbsmktelkom/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -40,6 +41,88 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doClick() {
+        if (isValid()) {
+            StringBuilder builder = new StringBuilder();
+            String nama = etnama.getText().toString();
+            String tmpLhr = ettmpLhr.getText().toString();
+            int thnLhr = Integer.parseInt(etthnLhr.getText().toString());
+            int usia = 2016 - thnLhr;
+            String hasil = null;
+            if (rgJK.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton) findViewById(rgJK.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            if (hasil == null) {
+                tvHasil.setText("Anda belum memilih Jenis Kelamin\n");
+            } else {
+                String asal = etasal.getText().toString();
+                String program = "Program yang dipilih \t\n";
+                int startlen = program.length();
+                if (cbrgl.isChecked()) program += "\t " + cbrgl.getText() + "\n";
+                if (cbblg.isChecked()) program += "\t " + cbrgl.getText() + "\n";
+                if (cbaksl.isChecked()) program += "\t " + cbrgl.getText() + "\n";
+                if (program.length() == startlen) {
+                    tvHasil.setText("Tidak ada Program yang dipilih\n");
+                } else {
+                    builder.append("\nPSB SMK TELKOM MALANG");
+                    builder.append("\n" + program);
+                    builder.append("Nama : " + nama + "\n");
+                    builder.append("Jenis Kelamin : " + hasil + "\n");
+                    builder.append("Tempat Lahir : " + tmpLhr + "\n");
+                    builder.append("Tahun Lahir : " + thnLhr + "\n");
+                    builder.append("Umur : " + usia + "\n");
+                    builder.append("Agama : ");
+                    builder.append(spagama.getSelectedItem().toString() + "\n");
+                    builder.append("Asal Sekolah : " + asal + "\n");
 
+                    tvHasil.setText(builder);
+                }
+            }
+        }
     }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etnama.getText().toString();
+        String tmpLhr = ettmpLhr.getText().toString();
+        String thnLhr = etthnLhr.getText().toString();
+        String asal = etasal.getText().toString();
+
+        if (nama.isEmpty()) {
+            etnama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etnama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etnama.setError(null);
+        }
+        if (tmpLhr.isEmpty()) {
+            ettmpLhr.setError("Tempat Lahir belum diisi");
+            valid = false;
+        } else {
+            ettmpLhr.setError(null);
+        }
+        if (thnLhr.isEmpty()) {
+            etthnLhr.setError("Tahun belum diisi");
+            valid = false;
+        } else if (thnLhr.length() != 4) {
+            etthnLhr.setError("Format Tahun bukan yyyy");
+            valid = false;
+        } else {
+            etthnLhr.setError(null);
+        }
+        if (asal.isEmpty()) {
+            etasal.setError("Asal Sekolah belum diisi");
+            valid = false;
+        } else if (asal.length() < 5) {
+            etasal.setError("Nama Asal Sekolah minimal 5 karakter");
+            valid = false;
+        } else {
+            etasal.setError(null);
+        }
+        return valid;
+    }
+
 }
